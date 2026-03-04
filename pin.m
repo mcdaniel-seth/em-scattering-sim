bb0c15a2754090e7c408b5894969418d27ffff6f
mcdaniel-seth <seth.mcdanie@gmail.com>
2026-03-04 17:23:01 -0600

    mar4commit

--- pin.m
+++ pin.m
@@ -2,8 +2,10 @@
 function pi_n = pin(n,theta)
 % n = index
 % theta angle input
-    P = legendre(n, cos(theta));
-    Pm1 = P(2,:);                    % always m = 1
-    pi_n = Pm1 ./ sin(theta);
-
+    
+    if (theta== 0 ) theta = 0.00001;
+        P = legendre(n, cos(theta));
+        Pm1 = P(2,:);                    % always m = 1
+        pi_n = Pm1 ./ sin(theta);
+    end
 end
